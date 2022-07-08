90876a9f260f500839d5e79f05188fe960a5f300
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2022-07-08 11:56:30 +0800

    chore: setup TypScript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const moduleResolver = [
+  'module-resolver',
+  {
+    root: ['./src'],
+    extensions: ['ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+    alias: {
+      '@src': ['./src/'],
+    },
+  },
+];
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [moduleResolver],
 };
